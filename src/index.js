0a1f2cd1cf745868f63d96005a6bcb1b2301e717
Hyago Caetano <hyagocae@gmail.com>
2022-08-28 18:51:19 -0300

    helloWorld

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+app.use(express())
+app.use(express.json())
+
+app.post('/', (req, res) => {
+    res.json('HELLOW WORLD')
+})
+
+app.listen(4488)
