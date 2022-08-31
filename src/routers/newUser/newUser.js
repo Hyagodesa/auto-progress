63fd7cc94f42e47718ee419b84b65812c506cbe5
Hyago Caetano <hyagocae@gmail.com>
2022-08-30 23:29:18 -0300

    veficando email  e enviando para lista de dados salvos

--- src/routers/newUser/newUser.js
+++ src/routers/newUser/newUser.js
@@ -1,13 +1,21 @@
+
 const express = require('express')
 
 const userRouter = express.Router()
-
+const list = []
+console.log(list)
 userRouter.post('/user', (req, res) => {
   const { name, email, password } = req.body
-  const user = { name, email, password }
-  if (user.name.length <= 3) return res.status(404).send('nome invalido')
-  if (!user.email.includes('@')) return res.status(404).send('email invalido')
-  if (user.password.length <= 7) return res.status(404).send('insira uma senha mais forte')
+  if (name.length <= 3) return res.status(404).send('nome invalido')
+  if (!email.includes('@')) return res.status(404).send('email invalido')
+  if (password.length <= 7) return res.status(404).send('insira uma senha mais forte')
+  const verify = list.find((user) => user === email)
+  if (verify) return res.send('usuario ja foi cadastrado')
+
   res.json({ NewUser: 'cadastrado' })
+})
+
+userRouter.get('/user', (req, res) => {
+  res.json(list)
 })
 module.exports = userRouter
