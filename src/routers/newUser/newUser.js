6005611992b7ff60dc026df429aaad609be9083c
Hyago Caetano <hyagocae@gmail.com>
2022-08-31 22:44:40 -0300

    dados.json

--- src/routers/newUser/newUser.js
+++ src/routers/newUser/newUser.js
@@ -1,14 +1,15 @@
 
 const express = require('express')
 
+
 const userRouter = express.Router()
 const list = []
 console.log(list)
 userRouter.post('/user', (req, res) => {
   const { name, email, password } = req.body
-  if (name.length <= 3) return res.status(404).send('nome invalido')
-  if (!email.includes('@')) return res.status(404).send('email invalido')
-  if (password.length <= 7) return res.status(404).send('insira uma senha mais forte')
+  if (name.length <= 3) return res.send('nome invalido')
+  if (!email.includes('@')) return res.send('email invalido')
+  if (password.length <= 7) return res.send('insira uma senha mais forte')
   const verify = list.find((user) => user === email)
   if (verify) return res.send('usuario ja foi cadastrado')
 
