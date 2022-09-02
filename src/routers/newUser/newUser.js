dfbe8e139e41eeca425b05ac867bb384c43dcf50
Hyago Caetano <hyagocae@gmail.com>
2022-09-01 23:04:40 -0300

    finalizado a função de verificação de usuario, push completo do usuario com ID e Date

--- src/routers/newUser/newUser.js
+++ src/routers/newUser/newUser.js
@@ -1,22 +1,21 @@
 
 const express = require('express')
-
+const idUtils = require('../../shared/utils/idUtility')
+const dateUtils = require('../../shared/utils/dateUtils')
 
 const userRouter = express.Router()
 const list = []
-console.log(list)
 userRouter.post('/user', (req, res) => {
   const { name, email, password } = req.body
   if (name.length <= 3) return res.send('nome invalido')
   if (!email.includes('@')) return res.send('email invalido')
   if (password.length <= 7) return res.send('insira uma senha mais forte')
-  const verify = list.find((user) => user === email)
+  const verify = list.find((user) => user.email === email)
   if (verify) return res.send('usuario ja foi cadastrado')
+  list.push({ name, email, password, id: idUtils.generate(), createdAt: dateUtils.now(), status: true })
 
   res.json({ NewUser: 'cadastrado' })
+  console.log(list)
 })
 
-userRouter.get('/user', (req, res) => {
-  res.json(list)
-})
 module.exports = userRouter
