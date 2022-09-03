f85141e828b8394334e81548ecc7a8cd29c26eaa
Hyago Caetano <hyagocae@gmail.com>
2022-09-02 22:15:56 -0300

    max caracteres

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -11,13 +11,13 @@ userRouter.post('/user', (req, res) => {
 
   if (!name) return res.status(400).send('Insira um nome.')
   if (!validator.minLength(name, 4)) return res.status(400).send('Insira um nome maior, com pelo menos 4 caracteres.')
-  if (!validator.maxLength(name, 30)) return res.status(400).send('Insira um nome menor, com no maximo30 caracteres.')
+  if (!validator.maxLength(name, 30)) return res.status(400).send('Insira um nome menor, com no máximo 30 caracteres.')
 
   if (!email) return res.status(400).send('Insira um email.')
   if (!validator.IsEmail(email)) return res.status(400).send('Email inválido.')
 
   if (!password) return res.status(400).send('Insira uma senha.')
-  if (!validator.length(password, 8, 20)) return res.status(400).send('Insira uma senha entre 8 e 20 caracteres')
+  if (!validator.length(password, 8, 20)) return res.status(400).send('Insira uma senha entre 8 e 20 caracteres.')
 
   const user = users.find((user) => user.email === email)
   if (user) return res.status(400).send('Este email já foi cadastrado.')
