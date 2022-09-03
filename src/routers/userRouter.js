45b16152d85fd83d93359f106f3d2632fd7ccd47
Hyago Caetano <hyagocae@gmail.com>
2022-09-02 21:39:30 -0300

    espaçamento para melhor leitura

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,12 +7,16 @@ userRouter.post('/user', (req, res) => {
   const { name, email, password } = req.body
   if (!name) return res.status(400).send('Insira um nome.')
   if (name.length <= 3) return res.status(400).send('Insira um nome maior, com pelo menos 4 caracteres.')
+
   if (!email) return res.status(400).send('Insira um email.')
-  if (!email.includes('@') || !email.includes('.com')) return res.status(400).send('Email invalido.')
+  if (!email.includes('@') || !email.includes('.com')) return res.status(400).send('Email inválido.')
+
   if (!password) return res.status(400).send('Insira uma senha.')
   if (password.length <= 7) return res.status(400).send('Insira uma senha mais forte, com pelo menos 8 caracteres.')
+
   const user = list.find((user) => user.email === email)
-  if (user) return res.status(400).send('Usuário ja foi cadastrado.')
+  if (user) return res.status(400).send('Este email já foi cadastrado.')
+
   list.push({ name, email, password, id: idUtils.generate(), createdAt: dateUtils.now(), status: true })
   return res.status(204).json()
 })
